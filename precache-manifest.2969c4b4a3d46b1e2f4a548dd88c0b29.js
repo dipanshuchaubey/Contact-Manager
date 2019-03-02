daaf3244148993696181c03f8f58deac7ab5696f
Dipanshu <dipanshuchaubey@gmail.com>
2019-03-02 18:51:26 +0530

    Updates

---
+++ precache-manifest.2969c4b4a3d46b1e2f4a548dd88c0b29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b8fdbf7579a38197298",
+    "url": "/contact-manager/static/js/runtime~main.2b8fdbf7.js"
+  },
+  {
+    "revision": "1751b9ef304a63feb400",
+    "url": "/contact-manager/static/js/main.1751b9ef.chunk.js"
+  },
+  {
+    "revision": "ae20536725fbbd7e72ad",
+    "url": "/contact-manager/static/js/1.ae205367.chunk.js"
+  },
+  {
+    "revision": "1751b9ef304a63feb400",
+    "url": "/contact-manager/static/css/main.23e8ff64.chunk.css"
+  },
+  {
+    "revision": "ae20536725fbbd7e72ad",
+    "url": "/contact-manager/static/css/1.76bb88f3.chunk.css"
+  },
+  {
+    "revision": "fb9986c3364661a9835cfc2e6c56fafc",
+    "url": "/contact-manager/index.html"
+  }
+];
